c818381d3e5e4456390d9585a90ca48945bf186e
hugessen <richard@hugessen.ca>
2016-12-03 21:04:49 -0500

    Excel Mode
    Changes output to purely the best result from last generation, repeated
    10,000 times.
    If excel mode is off, output is as normal

--- src/termproj/Driver.java
+++ src/termproj/Driver.java
@@ -11,16 +11,23 @@ public class Driver
     {
         int of_case = 1;
         int num_gen = 50;
-
-        if (of_case == 0)
-        {
-            QuadraticFunction of = new QuadraticFunction();
-            GA ga = new GA(num_gen, 60, 0, of, true);
-        }
-        else if (of_case == 1)
-        {
-            RosenbrockValley of = new RosenbrockValley(1, 100);
-            GA ga = new GA(num_gen, (int)Math.pow(2, 12)-1, 0, of, false);
-        }
+        boolean excel_mode = false;
+        int num_iterations;
+        if(excel_mode)
+        	num_iterations = 10000;
+        else
+        	num_iterations = 1;
+        
+        for(int i = 0; i < num_iterations; i++)
+	        if (of_case == 0)
+	        {
+	            QuadraticFunction of = new QuadraticFunction();
+	            GA ga = new GA(num_gen, 60, 0, of, true,excel_mode);
+	        }
+	        else if (of_case == 1)
+	        {
+	            RosenbrockValley of = new RosenbrockValley(1, 100);
+	            GA ga = new GA(num_gen, (int)Math.pow(2, 12)-1, 0, of, false,excel_mode);
+	        }
     }
 }
